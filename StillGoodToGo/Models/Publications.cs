bc0d4dd3fcd4817cbef673f7c5d041b022000ba3
Carlos Siqueira <carlosfernando.siq@gmail.com>
2025-02-28 15:51:46 +0000

    Created Publications Model

---
+++ StillGoodToGo/Models/Publications.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace StillGoodToGo.Models
+{
+    /// <summary>
+    /// Represents a publication made by an establishment.
+    /// </summary>
+    public class Publications
+    {
+        /// <summary>
+        /// Unique identifier for the publication.
+        /// </summary>
+        [Key]
+        public int Id { get; set; }
+
+        /// <summary>
+        /// Establishment associated with the publication.
+        /// </summary>
+        [Required]
+        public Establishment Establishment { get; set; }
+
+        /// <summary>
+        /// Description of the publication.
+        /// </summary>
+        [Required]
+        [MaxLength(100)]
+        public string? Description { get; set; }
+
+        /// <summary>
+        /// Price of the published item.
+        /// </summary>
+        [Required]
+        public double Price { get; set; }
+
+        /// <summary>
+        /// Posting date of the publication.
+        /// </summary>
+        [Required]
+        public DateTime PostDate { get; set; }
+
+        /// <summary>
+        /// Expiration date of the publication.
+        /// </summary>
+        [Required]
+        public DateTime EndDate { get; set; }
+
+        /// <summary>
+        /// Status of the publication (active or inactive).
+        /// </summary>
+        [Required]
+        public Boolean Status { get; set; }
+
+        /// <summary>
+        /// Constructor to create a new publication without an ID.
+        /// </summary>
+        /// <param name="establishment">Establishment responsible for the publication.</param>
+        /// <param name="description">Description of the publication.</param>
+        /// <param name="price">Price of the item.</param>
+        /// <param name="postDate">Posting date.</param>
+        /// <param name="endDate">Expiration date.</param>
+        /// <param name="status">Status of the publication.</param>
+        public Publications(Establishment establishment, string description, double price, DateTime postDate, DateTime endDate, Boolean status)
+        {
+            Establishment = establishment;
+            Description = description;
+            Price = price;
+            PostDate = postDate;
+            EndDate = endDate;
+            Status = status;
+        }
+
+        /// <summary>
+        /// Constructor to create a publication with a specific ID.
+        /// </summary>
+        /// <param name="id">Unique identifier of the publication.</param>
+        /// <param name="establishment">Establishment responsible for the publication.</param>
+        /// <param name="description">Description of the publication.</param>
+        /// <param name="price">Price of the item.</param>
+        /// <param name="postDate">Posting date.</param>
+        /// <param name="endDate">Expiration date.</param>
+        /// <param name="status">Status of the publication.</param>
+        public Publications(int id, Establishment establishment, string description, double price, DateTime postDate, DateTime endDate, Boolean status)
+        {
+            Id = id;
+            Establishment = establishment;
+            Description = description;
+            Price = price;
+            PostDate = postDate;
+            EndDate = endDate;
+            Status = status;
+        }
+    }
+}
